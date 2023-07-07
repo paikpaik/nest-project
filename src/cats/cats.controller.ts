f6eb530b8777ca062584901ecfe0db6796f2a0d5
paikpaik <woduf9311@gmail.com>
2023-07-07 17:33:50 +0900

    Feat: nestjs HttpExceptionFilter적용

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,37 +1,41 @@
-import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, UseFilters } from '@nestjs/common';
+import { HttpException, Patch, Post, Put } from '@nestjs/common';
 import { CatsService } from './cats.service';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
   getAllCat() {
-    return 'all cat';
+    throw new HttpException('api is broken', 401);
+    return 'get all cat api';
   }
 
   @Get(':id')
   getOneCat() {
-    return 'one cat';
+    return 'get one cat api';
   }
 
   @Post()
   createCat() {
-    return 'create cat';
+    return 'create cat api';
   }
 
   @Put(':id')
   updateCat() {
-    return 'update cat';
+    return 'update cat api';
   }
 
   @Patch(':id')
   updatePartialCat() {
-    return 'update';
+    return 'update partial cat api';
   }
 
   @Delete(':id')
   deleteCat() {
-    return 'delete service';
+    return 'delete service api';
   }
 }
