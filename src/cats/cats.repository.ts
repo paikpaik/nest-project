bbafa34e7ec71caba4f6be08d881120b59b07cd6
paikpaik <woduf9311@gmail.com>
2023-07-08 17:00:06 +0900

    Feat: Repository패턴추가, 레이어 분리

--- src/cats/cats.repository.ts
+++ src/cats/cats.repository.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Cat } from './cats.schema';
@@ -8,15 +8,9 @@ import { CatRequestDto } from './dto/cats.request.dto';
 export class CatsRepository {
   constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
 
-  // 현재 validation에서 에러처리가 잘 작동이 되므로 에러처리를 위한 try catch구문을 사용할 필요는 없음.
-  // 다만 필요하다면 에러처리는 이곳에서 try catch로 처리함.
   async existByEmail(email: string): Promise<boolean> {
-    try {
-      const result = await this.catModel.exists({ email });
-      return result ? true : false;
-    } catch (error) {
-      throw new HttpException('db error', 400);
-    }
+    const result = await this.catModel.exists({ email });
+    return result ? true : false;
   }
 
   async create(cat: CatRequestDto): Promise<Cat> {
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,17 +1,15 @@
+import { CatsRepository } from './cats.repository';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CatRequestDto } from './dto/cats.request.dto';
-import { InjectModel } from '@nestjs/mongoose';
-import { Cat } from './cats.schema';
-import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class CatsService {
-  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+  constructor(private readonly catsRepository: CatsRepository) {}
 
   async signUp(body: CatRequestDto) {
     const { email, password, name } = body;
-    const isCatExist = await this.catModel.exists({ email });
+    const isCatExist = await this.catsRepository.existByEmail(email);
 
     if (isCatExist) {
       throw new UnauthorizedException('해당 고양이는 이미 존재합니다.');
@@ -19,7 +17,7 @@ export class CatsService {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const cat = await this.catModel.create({
+    const cat = await this.catsRepository.create({
       email,
       password: hashedPassword,
       name,
