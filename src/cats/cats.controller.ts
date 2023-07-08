0d3e7a76f04dedf7fb441cf6e51c2fcb7ec18097
paikpaik <woduf9311@gmail.com>
2023-07-08 13:37:27 +0900

    Feat: mongodb 스키마설계, validation추가, 컨트롤러코드 수정

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,13 +1,5 @@
-import {
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  UseFilters,
-  UseInterceptors,
-} from '@nestjs/common';
-import { Patch, Post, Put } from '@nestjs/common';
+import { Controller, Get, UseFilters, UseInterceptors } from '@nestjs/common';
+import { Post } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
 import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
@@ -19,35 +11,27 @@ export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
-  getAllCat() {
-    console.log('hello controller');
-    return 'get all cat api';
-  }
-
-  @Get(':id')
-  getOneCat(@Param('id', ParseIntPipe) param: number) {
-    console.log(param);
-    console.log(typeof param);
-    return 'get one cat api';
+  getCurrentCat() {
+    return 'current cat';
   }
 
   @Post()
-  createCat() {
-    return 'create cat api';
+  async signup() {
+    return 'signup';
   }
 
-  @Put(':id')
-  updateCat() {
-    return 'update cat api';
+  @Post('login')
+  logIn() {
+    return 'login';
   }
 
-  @Patch(':id')
-  updatePartialCat() {
-    return 'update partial cat api';
+  @Post('logout')
+  logOut() {
+    return 'logout';
   }
 
-  @Delete(':id')
-  deleteCat() {
-    return 'delete service api';
+  @Post('upload/cats')
+  uploadCatImg() {
+    return 'uploadImg';
   }
 }
